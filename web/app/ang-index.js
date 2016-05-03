d65ebf126dd21357941a03cef29e88f9e2d1611c
vrom911 <vrom911@gmail.com>
2016-05-03 18:29:25 +0400

    Testing some pages

--- web/app/ang-index.js
+++ web/app/ang-index.js
@@ -1,80 +1,92 @@
 (function () {
         var app = angular.module("project", []);
 
+//        app.controller('DashboardCtrl', function ($scope, $state) {
+//                $scope.$state = $state;
+//
+//                $scope.menuItems = [];
+//                angular.forEach($state.get(), function (item) {
+//                        if (item.data && item.data.visible) {
+//                                $scope.menuItems.push({name: item.name, text: item.data.text});
+//                        }
+//                });
+//        });
 
-        app.directive("navigationBar", function () {
-                return {
-                        restrict: "E",
-                        templateUrl: "app/templates/navbar-menu.html",
-                        controller: function () {
 
-                                this.navigation = navigation;
-                        },
-                        controllerAs: "navbar"
-                };
-        });
-        app.directive("pictureModals", function () {
+//        app.directive("navigationBar", function () {
+//                return {
+//                        restrict: "E",
+//                        templateUrl: "/templates/navbar-menu.html",
+//                        controller: function () {
+//
+//                                this.navigation = navigation;
+//                        },
+//                        controllerAs: "navbar"
+//                };
+//        });
+        app.directive("pictureModals", function() {
                 return {
                         restrict: "E",
                         templateUrl: "app/templates/picture-modals.html",
-                        controller: function () {
+                        controller: function() {
                                 this.data = data;
                         },
                         controllerAs: "picmod"
                 };
         });
-        app.directive("services", function () {
+        app.directive("services", function() {
                 return {
                         restrict: "E",
                         templateUrl: "app/templates/services.html",
-                        controller: function () {
+                        controller: function() {
+                                console.log("service");
                                 this.data = service;
                         },
                         controllerAs: "services"
                 };
         });
-        app.directive("sendMessage", function () {
+        app.directive("sendMessage", function() {
                 return {
                         restrict: "E",
                         templateUrl: "app/templates/send-message.html",
-                        controller: function () {
-                                this.submitForm = function (check) {
+                        controller: function() {
+                                this.submitForm = function(check) {
                                         if (check) {
                                                 alert("Thanks for the message!");
                                         }
-                                        
+
                                 };
                         },
                         controllerAs: "form"
                 };
         });
-        app.directive("teamMembers", function () {
+        app.directive("teamMembers", function() {
                 return {
                         restrict: "E",
                         templateUrl: "app/templates/our-team.html",
-                        controller: function () {
+                        controller: function() {
 
                                 this.members = ourTeam;
                         },
                         controllerAs: "team"
                 };
         });
-        app.directive("timelineSteps", function () {
+        app.directive("timelineSteps", function() {
                 return {
                         restrict: "E",
                         templateUrl: "app/templates/timeline.html",
-                        controller: function () {
+                        controller: function() {
 
                                 this.steps = steps;
                         },
                         controllerAs: "timeline"
                 };
         });
-        app.directive("socialLinks", function () {
+        app.directive("socialLinks", function() {
                 return {
                         restrict: "E",
                         templateUrl: "app/templates/social-links.html",
-                        controller: function () {
+                        controller: function() {
 
                                 this.slinks = slinks;
                         },
